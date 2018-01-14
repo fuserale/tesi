5ebe36ccff6094ead14ef91eba513a6655a363f4
fuserale <alessandrofsr@gmail.com>
2018-01-14 16:28:13 +0100

    14/01

---
+++ FeatureFusion/extract_prefog.m
@@ -0,0 +1,34 @@
+function extract_prefog
+
+fileruns = dir('3cl_*.csv');
+for r = 1:length(fileruns)
+    T = readtable(fileruns(r).name);
+    
+    [m,~] = size(T);
+    A = table2array(T);
+    Fs = 64;
+    size_windows_sec = 2;
+    %size of the windows in number of samples
+    size_windows_sample = Fs * size_windows_sec;
+    
+    i = 1;
+    while i < m
+        %disp(T.Var11);
+        if A(i,11) == 2
+            for l = i-(size_windows_sample):i-1
+                if A(l,11) ~= 2
+                    A(l,11) = 3;
+                end
+            end
+            
+            while (A(i,11) == 2 && i < m )
+                i = i + 1;
+            end
+        end
+        i = i + 1;
+    end
+    T = array2table(A);
+    writetable(T, [fileruns(r).name]);
+    display(fileruns(r).name);
+end
+end
