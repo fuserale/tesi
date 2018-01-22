e6514842158a598fcb0b2cab6a924ba2379e4a26
fuserale <alessandrofsr@gmail.com>
2018-01-22 22:27:08 +0100

    22/01

--- FeatureFusion/PatientUnion.m
+++ FeatureFusion/PatientUnion.m
@@ -1,28 +1,22 @@
 clear; clc
 
 A = [];
-%choose number of patients to examine (from 1 to 10)
-for isubject = [1 2 3 4 5 6 7 8 9]
+datadir = '';
+
+for isubject = [2 3 4 5 6 7 8 9 10]
     
-    %list of all files for patient number $isubject
-    fileruns = dir(['3cl_S' num2str(isubject,'%02d') 'R01.csv']);
-    
-    %while there's file of patient $isubject
+    fileruns = dir([datadir 'S' num2str(isubject,'%02d') 'R01.csv']);
+
     for r = 1:length(fileruns)
         
-        %name of the file
         filename = fileruns(r).name;
-        %read table given in input
-        T = readtable(filename);
-        %take the dimesion
-        [m,n] = size(T);
-        %table to array to do maths
-        B = table2array(T);
+        B = table2array(readtable(filename));
         
         A = [A ; B];
         
     end
 end
+
 P = array2table(A);
-writetable(P, 'leaveout_10.csv');
-disp('leaveout_10.csv');
+writetable(P, [datadir '2cl_leaveout_1.csv']);
+disp([datadir '2cl_leaveout_1.csv']);
--- FeatureFusion/main.m
+++ FeatureFusion/main.m
@@ -3,9 +3,10 @@ clear;
 close all;
 
 %% Load Data
+datadir = 'dataset_2cl/';
 %T = readtable('3cl_S01R01.csv');
-x = 1;
-T = readtable(['leaveout_' num2str(x, '%01d') '.csv']);
+x = 01;
+T = readtable([datadir '2cl_leaveout_' num2str(x, '%01d') '.csv']);
 [m,n] = size(T);
 A = table2array(T(:,2:10));
 TIME = table2array(T(:,1));
@@ -32,100 +33,104 @@ for i=1:size_overlap_samples:m - size_windows_sample
     number_sample = number_sample + 1;
     
 end
-C = [F,class'];
+% C = [F,class'];
 
-%% Codice LDA_KNN2
-%
-% FREEZE
-% 1 non fog
-% 2 fog
-% 3 prefog
-
-
-% perchè in A ho i dati cosi ogni colonna corrisponde ad un accelerometro, li prendo tutti
-clear A;
-A=F';
-
-%quindi ho A 1152x1449 double: 1152 finestre ed ogni finestra ha 1449 features
-[d,N] = size(A);
-
-K =  max(class); % numero classi in gioco;
-
-% 1. determino le classi Ck
-for k = 1:K
-    a = find (class== k);
-    Ck{k} = A(:, a);   
-end
-
-% 2. determino le medie
-for k = 1:K
-    mk{k} = mean(Ck{k},2);
-end
-% 3. determino la numerosità della classe
-for k = 1:K
-    [d, Nk(k)] = size(Ck{k});
-end
-% 4. determino le within class covariance
-for k = 1:K
-    S{k} = 0;
-    for i = 1:Nk(k)
-        S{k} = S{k} + (Ck{k}(:,i)-mk{k})*(Ck{k}(:,i)-mk{k})';
-    end
-    S{k} = S{k}./Nk(k);
-end
-Swx = 0;
-for k = 1:K
-    Swx = Swx + S{k};
-end
-
-% 5. determino la between class covariance
-% 5.1 determino la media totale
-m = mean(A,2);
-Sbx = 0;
-for k=1:K
-    Sbx = Sbx + Nk(k)*((mk{k} - m)*(mk{k} - m)');
-end
-Sbx = Sbx/K;
-
-MA = inv(Swx)*Sbx;
-
-% eigenvalues/eigenvectors
-[V,D] = eig(MA);
-
-% 5: transform matrix
-W = V(:,1:2);
-
-% 6: transformation
-Y = W'*A;
-
-% 7: plot
-figure, gscatter(Y(1,:),Y(2,:),class)
-legend('NoFog','Fog','PreFog')
+% %% Codice LDA_KNN2
+% %
+% % FREEZE
+% % 1 non fog
+% % 2 fog
+% % 3 prefog
+% 
+% 
+% % perchè in A ho i dati cosi ogni colonna corrisponde ad un accelerometro, li prendo tutti
+% clear A;
+% A=F';
+% 
+% %quindi ho A 1152x1449 double: 1152 finestre ed ogni finestra ha 1449 features
+% [d,N] = size(A);
+% 
+% K =  max(class); % numero classi in gioco;
+% 
+% % 1. determino le classi Ck
+% for k = 1:K
+%     a = find (class== k);
+%     Ck{k} = A(:, a);   
+% end
+% 
+% % 2. determino le medie
+% for k = 1:K
+%     mk{k} = mean(Ck{k},2);
+% end
+% % 3. determino la numerosità della classe
+% for k = 1:K
+%     [d, Nk(k)] = size(Ck{k});
+% end
+% % 4. determino le within class covariance
+% for k = 1:K
+%     S{k} = 0;
+%     for i = 1:Nk(k)
+%         S{k} = S{k} + (Ck{k}(:,i)-mk{k})*(Ck{k}(:,i)-mk{k})';
+%     end
+%     S{k} = S{k}./Nk(k);
+% end
+% Swx = 0;
+% for k = 1:K
+%     Swx = Swx + S{k};
+% end
+% 
+% % 5. determino la between class covariance
+% % 5.1 determino la media totale
+% m = mean(A,2);
+% Sbx = 0;
+% for k=1:K
+%     Sbx = Sbx + Nk(k)*((mk{k} - m)*(mk{k} - m)');
+% end
+% Sbx = Sbx/K;
+% 
+% MA = inv(Swx)*Sbx;
+% 
+% % eigenvalues/eigenvectors
+% [V,D] = eig(MA);
+% 
+% % 5: transform matrix
+% W = V(:,1:2);
+% 
+% % 6: transformation
+% Y = W'*A;
+% 
+% % 7: plot
+% figure, gscatter(Y(1,:),Y(2,:),class)
+% legend('NoFog','Fog')
+% % legend('NoFog','Fog','PreFog')
 
 %% Default linear discriminant analysis (LDA)
-lda = fitcdiscr(F,class')
+lda = fitcdiscr(F,class');
 ldaClass = resubPredict(lda);
 ldaResubErr = resubLoss(lda)
 [ldaResubCM,grpOrder] = confusionmat(class',ldaClass)
+% figure, gscatter(lda.Mu(1,:), lda.Mu(2,:), class');
+% legend('NoFog','Fog');
 
 %% Predizione su altri dati
-clear F;
-T = readtable('3cl_S01R01.csv');
-[m,n] = size(T);
-A = table2array(T(:,2:10));
-TIME = table2array(T(:,1));
-FREEZE = table2array(T(:,11));
+F = [];
+T2 = readtable([datadir 'S' num2str(x, '%02d') 'R01.csv']);
+[m,n] = size(T2);
+A2 = table2array(T2(:,2:10));
+TIME2 = table2array(T2(:,1));
+FREEZE2 = table2array(T2(:,11));
 Fs = 64;
+number_sample = 1;
 for i=1:size_overlap_samples:m - size_windows_sample
-    B = A(i:i+size_windows_sample-1,:);
-    [m,~] = size(B);
+    B2 = A2(i:i+size_windows_sample-1,:);
+    [m,~] = size(B2);
     
-    F(number_sample, :) = B(:);
-    class(number_sample) = mode(FREEZE(i:i+size_windows_sample-1,:));
+    F2(number_sample, :) = B2(:);
+    class2(number_sample) = mode(FREEZE2(i:i+size_windows_sample-1,:));
     
     number_sample = number_sample + 1;
     
 end
 
-label = predict(lda, F);
-[ldaResubCM,grpOrder] = confusionmat(class',label)
+label = predict(lda, F2);
+[ldaResubCM,grpOrder] = confusionmat(class2',label)
